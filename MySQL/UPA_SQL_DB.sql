fd8f3872ab041488bd79f3ba54c1415a91e33ed8
Radomír Stojan <xstoja07@stud.fit.vutbr.cz>
2020-12-04 16:58:42 +0100

    Updated MySQL Db to match ERD

--- MySQL/UPA_SQL_DB.sql
+++ MySQL/UPA_SQL_DB.sql
@@ -9,12 +9,13 @@ ALTER DATABASE UPA_SQL_Db
 
 -- ----------------- DROP TABLES ---------------------
 
-DROP TABLE IF EXISTS Station;
 DROP TABLE IF EXISTS SelectedWeatherReport;
+DROP TABLE IF EXISTS Station;
 
 -- ----------------- CREATE TABLES ---------------------
 
 CREATE TABLE Station (WMO_ID INTEGER PRIMARY KEY NOT NULL,
+                        Timezone VARCHAR(100),
                         Latitude FLOAT NOT NULL,
                         Longitude FLOAT NOT NULL,
                         TemperatureMedian FLOAT,
@@ -36,14 +37,14 @@ CREATE TABLE SelectedWeatherReport (AUTO_ID INTEGER AUTO_INCREMENT PRIMARY KEY N
 
 -- ----------------- TESTING DATA SETS ---------------------
 
-INSERT INTO Station(WMO_ID, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
-             VALUES (0, 100.12, -56.378, 28.7, 22.5, 27.9, 37.6, 2);
-INSERT INTO Station(WMO_ID, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
-             VALUES (1, 56098.1789, -523326.371318, 30.0, 23.8, 29.4, 25.6, 0.0);
-INSERT INTO Station(WMO_ID, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
-             VALUES (2, -1265, -16131.3578, 25.3, 19.8, 24.2, 57, 12.7);
-INSERT INTO Station(WMO_ID, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
-             VALUES (3, -0.1312, 12156.378, 34.5, 27.1, 33.9, 15.6, 0.2);
+INSERT INTO Station(WMO_ID, Timezone, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
+             VALUES (0, "Australia", 100.12, -56.378, 28.7, 22.5, 27.9, 37.6, 2);
+INSERT INTO Station(WMO_ID, Timezone, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
+             VALUES (1, "Finland", 56098.1789, -523326.371318, 30.0, 23.8, 29.4, 25.6, 0.0);
+INSERT INTO Station(WMO_ID, Timezone, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
+             VALUES (2, "Russia", -1265, -16131.3578, 25.3, 19.8, 24.2, 57, 12.7);
+INSERT INTO Station(WMO_ID, Timezone, Latitude, Longitude, TemperatureMedian, TemperatureNightMedian, TemperatureDayMedian, HumidityMedian, RainfallMedian)
+             VALUES (3, "France", -0.1312, 12156.378, 34.5, 27.1, 33.9, 15.6, 0.2);
 
 INSERT INTO SelectedWeatherReport(Station_ID, Pressure, Humidity, Rainfall)
              VALUES (0, 1054.26, 37.78, 2.4);

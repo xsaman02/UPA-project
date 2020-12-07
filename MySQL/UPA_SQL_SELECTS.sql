37eb4d21acdf107aa2cc2c5d0fecb3edcd145bc7
Radomír Stojan <xstoja07@stud.fit.vutbr.cz>
2020-12-07 19:59:27 +0100

    Fixed typo, updated SELECT for assignment A).

--- MySQL/UPA_SQL_SELECTS.sql
+++ MySQL/UPA_SQL_SELECTS.sql
@@ -1,40 +1,64 @@
 create database if not exists UPA_SQL_Db;
 use UPA_SQL_Db;
 
--- ----------------- SELECTS ---------------------
 
--- SELECT Station.* FROM Station;
--- SELECT "" as " ";
-
--- SELECT SelectedWeatherReport.* FROM SelectedWeatherReport;
--- SELECT "" as " ";
-
--- ----------------- SELECT A: ---------------------
+-- ----------------- SELECT A: ALL DATA ---------------------
 
 -- SELECT "" AS "--------------------------- SELECT A: Temperature START ----------------------------";
--- SELECT Station.WMO_ID, Station.Timezone, Station.Latitude, Station.Longitude, Station.TemperatureMedian FROM Station
+-- SELECT Station.WMO_ID, Station.StationName, Station.Latitude, Station.Longitude, Station.TemperatureMedian FROM Station
 -- WHERE TemperatureMedian != "NULL"
 -- ORDER BY TemperatureMedian DESC;
 -- SELECT "" AS "--------------------------- SELECT A: Temperature END ----------------------------";
 -- SELECT "" AS "--------------------------- SELECT A: Rainfall START ----------------------------";
--- SELECT Station.WMO_ID, Station.Timezone, Station.Latitude, Station.Longitude, Station.RainfallMean FROM Station
+-- SELECT Station.WMO_ID, Station.StationName, Station.Latitude, Station.Longitude, Station.RainfallMean FROM Station
 -- WHERE RainfallMean != "NULL"
 -- ORDER BY RainfallMean;
 -- SELECT "" AS "--------------------------- SELECT A: Rainfall END ----------------------------";
 
 
--- ----------------- SELECT B: ---------------------
+-- ----------------- SELECT A: SELECTED DATA ---------------------
+
+-- ----------------- SELECT A: TOP 20 ---------------------
+SELECT "" AS "--------------------------- SELECT A: Top 20 Temperature START ----------------------------";
+SELECT Station.WMO_ID, Station.StationName, Station.Latitude, Station.Longitude, Station.TemperatureMedian FROM Station
+WHERE TemperatureMedian != "NULL"
+ORDER BY TemperatureMedian DESC
+LIMIT 20;
+SELECT "" AS "--------------------------- SELECT A: Top 20 Temperature END ----------------------------";
+SELECT "" AS "--------------------------- SELECT A: Top 20 Rainfall START ----------------------------";
+SELECT Station.WMO_ID, Station.StationName, Station.Latitude, Station.Longitude, Station.RainfallMean FROM Station
+WHERE RainfallMean != "NULL"
+ORDER BY RainfallMean DESC
+LIMIT 20;
+SELECT "" AS "--------------------------- SELECT A: Top 20 Rainfall END ----------------------------";
+
+-- ----------------- SELECT A: LAST 20 ---------------------
+SELECT "" AS "--------------------------- SELECT A: Last 20 Temperature START ----------------------------";
+SELECT Station.WMO_ID, Station.StationName, Station.Latitude, Station.Longitude, Station.TemperatureMedian FROM Station
+WHERE TemperatureMedian != "NULL"
+ORDER BY TemperatureMedian ASC
+LIMIT 20;
+SELECT "" AS "--------------------------- SELECT A: Last 20 Temperature END ----------------------------";
+SELECT "" AS "--------------------------- SELECT A: Last 20 Rainfall START ----------------------------";
+SELECT Station.WMO_ID, Station.StationName, Station.Latitude, Station.Longitude, Station.RainfallMean FROM Station
+WHERE RainfallMean != "NULL"
+ORDER BY RainfallMean ASC
+LIMIT 20;
+SELECT "" AS "--------------------------- SELECT A: Last 20 Rainfall END ----------------------------";
+
+
+----------------- SELECT B: ---------------------
 
 SELECT Station.Latitude, Station.TemperatureDayMedian, Station.TemperatureNightMedian FROM Station
 WHERE Station.TemperatureDayMedian != "NULL" AND Station.TemperatureNightMedian != "NULL"
 ORDER BY Station.Latitude;
 
 
--- ----------------- SELECT C: ---------------------
+----------------- SELECT C: ---------------------
 
--- SELECT "" AS "--------------------------- SELECT C: START ----------------------------";
--- SELECT Station.WMO_ID, SelectedWeatherReport.Pressure, SelectedWeatherReport.Humidity, SelectedWeatherReport.Rainfall FROM Station
--- INNER JOIN SelectedWeatherReport ON Station.WMO_ID = SelectedWeatherReport.Station_ID
--- WHERE Rainfall != "NULL" AND Humidity != "NULL" AND Pressure != "NULL"
--- ORDER BY Pressure;
--- SELECT "" AS "--------------------------- SELECT C: END ----------------------------";
+SELECT "" AS "--------------------------- SELECT C: START ----------------------------";
+SELECT Station.WMO_ID, SelectedWeatherReport.Pressure, SelectedWeatherReport.Humidity, SelectedWeatherReport.Rainfall FROM Station
+INNER JOIN SelectedWeatherReport ON Station.WMO_ID = SelectedWeatherReport.Station_ID
+WHERE Rainfall != "NULL" AND Humidity != "NULL" AND Pressure != "NULL"
+ORDER BY Pressure;
+SELECT "" AS "--------------------------- SELECT C: END ----------------------------";
